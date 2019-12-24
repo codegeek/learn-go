5ab7c8621a2f366daa7569a427aa41c2826f55d4
Guillermo Castro <39493477+CodeGeek@users.noreply.github.com>
2019-12-24 13:44:44 -0600

    Mocking last pass

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -16,11 +16,13 @@ type Sleeper interface {
 	Sleep()
 }
 
-type DefaultSleeper struct {
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
 }
 
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
 
 func Countdown(out io.Writer, sleeper Sleeper) {
@@ -28,11 +30,12 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
+
 	sleeper.Sleep()
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
