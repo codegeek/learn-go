d35bd60c91d5aee79fa2a2e3165e4a88b2c78244
Guillermo Castro <39493477+CodeGeek@users.noreply.github.com>
2019-12-23 21:58:34 -0600

    Slices with tests

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,9 +1,29 @@
 package arrays
 
-func Sum(numbers []int) int {
-	sum := 0
+// Sum returns the sum of all integers in the slice
+func Sum(numbers []int) (sum int) {
 	for _, number := range numbers {
 		sum += number
 	}
-	return sum
+	return
+}
+
+// SumAll returns an array containing the sum of each array
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return
+}
+
+func SumAllTails(numbersToTail ...[]int) (tails []int) {
+	for _, numbers := range numbersToTail {
+		if len(numbers) == 0 {
+			tails = append(tails, 0)
+		} else {
+			tail := numbers[1:]
+			tails = append(tails, Sum(tail))
+		}
+	}
+	return
 }
