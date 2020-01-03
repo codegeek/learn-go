adf09bb4ba53ddbbbda2c3a5255d5d22c320739c
Guillermo Castro <39493477+CodeGeek@users.noreply.github.com>
2020-01-03 16:34:06 -0600

    Refactor

--- maths/clockface.go
+++ maths/clockface.go
@@ -1,6 +1,8 @@
 package clockface
 
 import (
+	"fmt"
+	"io"
 	"math"
 	"time"
 )
@@ -11,17 +13,24 @@ const (
 	clockCenterY     = 150
 )
 
+func SVGWriter(w io.Writer, t time.Time) {
+	io.WriteString(w, svgStart)
+	io.WriteString(w, bezel)
+	secondHand(w, t)
+	io.WriteString(w, svgEnd)
+}
+
 type Point struct {
 	X float64
 	Y float64
 }
 
-func SecondHand(t time.Time) Point {
+func secondHand(w io.Writer, t time.Time) {
 	p := secondHandPoint(t)
 	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // scale
 	p = Point{p.X, -p.Y}                                      // flip
 	p = Point{p.X + clockCenterX, p.Y + clockCenterY}         // translate
-	return p
+	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
 }
 
 func secondsInRadians(t time.Time) float64 {
@@ -34,3 +43,13 @@ func secondHandPoint(t time.Time) Point {
 	y := math.Cos(angle)
 	return Point{x, y}
 }
+
+const svgStart = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg xmlns="http://www.w3.org/2000/svg"
+     width="100%"
+     height="100%"
+     viewBox="0 0 300 300">`
+
+const bezel = `<circle cx="150" cy="150" r="100" style="fill:#fff;stroke:#000;stroke-width:5px;"/>`
+
+const svgEnd = `</svg>`
