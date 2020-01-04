c2dbf4ead16fc963fcb3145c8b7fceaa9ecd65b8
Guillermo Castro <39493477+CodeGeek@users.noreply.github.com>
2020-01-04 00:16:11 -0600

    Refactors and integration test

---
+++ http-server/InMemoryPlayerStore.go
@@ -0,0 +1,31 @@
+package main
+
+import "sync"
+
+// InMemoryPlayerStore collects data about players in memory
+type InMemoryPlayerStore struct {
+	store map[string]int
+	lock  sync.RWMutex
+}
+
+// NewInMemoryPlayerStore initializes an empty player store
+func NewInMemoryPlayerStore() *InMemoryPlayerStore {
+	return &InMemoryPlayerStore{
+		map[string]int{},
+		sync.RWMutex{},
+	}
+}
+
+// GetPlayerScore retrieves scores for a given player
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	i.lock.RLock()
+	defer i.lock.RUnlock()
+	return i.store[name]
+}
+
+// RecordWin will record a player's win
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.lock.Lock()
+	defer i.lock.Unlock()
+	i.store[name]++
+}
