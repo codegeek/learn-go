7c9979d7f6e85827381772b304018036accf25e1
Guillermo Castro <39493477+CodeGeek@users.noreply.github.com>
2020-01-03 18:07:33 -0600

    HTTP Server first pass

---
+++ http-server/server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
+}
+
+func GetPlayerScore(name string) string {
+	switch name {
+	case "Pepper":
+		return "20"
+	case "Floyd":
+		return "10"
+	default:
+		return ""
+	}
+}
